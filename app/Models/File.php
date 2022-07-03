bcbf67f5dfc149fa0bcfa4a7e03d9ff28a4f50f4
Haleem Hosein <haleemhosein@yahoo.com>
2022-07-02 20:46:10 -0400

    14 Human readable file sizes

--- app/Models/File.php
+++ app/Models/File.php
@@ -13,6 +13,19 @@ class File extends Model
         'path'
     ];
 
+    public function sizeForHumans()
+    {
+        $bytes = $this->size;
+
+        $units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
+
+        for ($i = 0; $bytes > 1024; $i++) {
+            $bytes /= 1024;
+        }
+
+        return round($bytes, 1) . ' ' . $units[$i];
+    }
+
     public static function booted(): void
     {
         static::creating(function ($model) {
